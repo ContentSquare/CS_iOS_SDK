3c87e52566ccc7e36126290b5881679e40ec1e7b
WANG Jie <2981971+wangjiejacques@users.noreply.github.com>
2021-09-07 16:50:31 +0200

    Support package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CS_iOS_SDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CS_iOS_SDK",
+            targets: ["Contentsquare"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+              name: "SwiftProtobuf",
+              url: "https://github.com/apple/swift-protobuf.git",
+              "1.15.0" ..< "2.0.0"
+            ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "Contentsquare", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.2/ContentsquareModule.xcframework.zip", checksum: "b99b03b31af43bce20da0e4a3f75722548392a0e9266cbe23867ea2b5b87006b")    ]
+)
