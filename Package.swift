4607344113e69a373c0a3d738fb82ce3318e3a9c
WANG Jie <2981971+wangjiejacques@users.noreply.github.com>
2021-09-07 17:53:18 +0200

    fix target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ContentsquareModule",
-            targets: ["Contentsquare"]),
+            targets: ["ContentsquareModule"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,5 +23,5 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "Contentsquare", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.2/ContentsquareModule.xcframework.zip", checksum: "b99b03b31af43bce20da0e4a3f75722548392a0e9266cbe23867ea2b5b87006b")    ]
+        .binaryTarget(name: "ContentsquareModule", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.2/ContentsquareModule.xcframework.zip", checksum: "b99b03b31af43bce20da0e4a3f75722548392a0e9266cbe23867ea2b5b87006b")    ]
 )
