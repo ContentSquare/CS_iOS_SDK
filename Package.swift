688cbb21b7b837924f63c84b579017557d387cc9
WANG Jie <2981971+wangjiejacques@users.noreply.github.com>
2021-09-14 17:52:09 +0200

    fix denpendency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,13 @@ let package = Package(
             ),
     ],
     targets: [
+        
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ContentsquareModule", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.2/ContentsquareModule.xcframework.zip", checksum: "b99b03b31af43bce20da0e4a3f75722548392a0e9266cbe23867ea2b5b87006b")    ]
+        .binaryTarget(name: "ContentsquareModule", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.2/ContentsquareModule.xcframework.zip", checksum: "b99b03b31af43bce20da0e4a3f75722548392a0e9266cbe23867ea2b5b87006b"),
+        .target(name: "ContentsquareModuleTarget", dependencies: [
+            .target(name: "ContentsquareModule"),
+            "SwiftProtobuf"
+        ], path: "ContentsquareModuleTarget")
+    ]
 )
