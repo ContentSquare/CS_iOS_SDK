5891f42ce6e08014bd7e104bfbcd2798359f2657
WANG Jie <2981971+wangjiejacques@users.noreply.github.com>
2021-09-16 17:27:00 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -5,29 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "CS_iOS_SDK",
+    platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ContentsquareModule",
-            targets: ["ContentsquareModuleTarget"]),
+            targets: ["ContentsquareModuleWrapper"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(
-              name: "SwiftProtobuf",
-              url: "https://github.com/apple/swift-protobuf.git",
-              "1.15.0" ..< "2.0.0"
-            ),
+            name: "SwiftProtobuf",
+            url: "https://github.com/apple/swift-protobuf.git",
+            "1.15.0" ..< "2.0.0"),
     ],
     targets: [
-        
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ContentsquareModule", url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.9/ContentsquareModule.xcframework.zip", checksum: "37e8b0b7f1331dc9cca5279123f80fac887ff2ae27d4c0b57738e397c7061894"),
-        .target(name: "ContentsquareModuleTarget", dependencies: [
-            .target(name: "ContentsquareModule"),
-            "SwiftProtobuf"
-        ], path: "ContentsquareModuleTarget")
+        // binaryTarget doesn't support dependency, use a wrapper to fix this.
+        .target(
+            name: "ContentsquareModuleWrapper",
+            dependencies: [
+                .target(name: "ContentsquareModule"),
+                "SwiftProtobuf"
+            ],
+            path: "ContentsquareModuleWrapper"),
+        .binaryTarget(
+            name: "ContentsquareModule",
+            url: "https://github.com/wangjiejacques/CS_iOS_SDK/releases/download/4.4.14/ContentsquareModuleStatic.xcframework.zip",
+            checksum: "2f9a5a62d2c238827d1fec8765ae33c07b861f06bada17f30d677ed0dc3277a7"),
     ]
 )
